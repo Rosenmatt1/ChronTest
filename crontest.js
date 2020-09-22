30ca59b77e01c360738199db2dd16cf9bc14c538
Mateo <rosenmatt1@gmail.com>
2020-09-22 09:34:26 -0600

    Added a test chron set interval in a function

--- crontest.js
+++ crontest.js
@@ -4,3 +4,14 @@ var job = new CronJob('0 0 23 20 9 0', function() {
   console.log('You will see this message every second');
 }, null, true, 'America/Los_Angeles');
 job.start();
+
+console.log("Logging crontest.js")
+
+testChron = () => {
+    this.interval = setInterval(() => {
+        console.log("test cron")
+   }, 300)
+ }
+
+ console.log(testChron())
+
