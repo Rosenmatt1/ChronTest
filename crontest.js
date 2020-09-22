baa62c0be491f564d9d3f26bfa6925b3461f93d5
Mateo <rosenmatt1@gmail.com>
2020-09-21 20:47:10 -0600

    Made a index.html to render to the browser

--- crontest.js
+++ crontest.js
@@ -1,6 +1,6 @@
 var CronJob = require('cron').CronJob;
 
-var job = new CronJob('* * * * * *', function() {
+var job = new CronJob('0 0 23 20 9 0', function() {
   console.log('You will see this message every second');
 }, null, true, 'America/Los_Angeles');
 job.start();
